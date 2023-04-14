2b1d3fd87da36923e9ca5a44e45fbf10eb9a51ea
Yasmin haidar <yasmin1635@hotmail.com>
2023-04-14 15:13:02 +0100

    MVP completed

--- safari_queries.sql
+++ safari_queries.sql
@@ -1,1 +1,8 @@
 SELECT * FROM animals WHERE enclosure_id = 1;
+
+SELECT employees.name, enclosures.name FROM employees
+INNER JOIN assignments
+ON  employees.id = assignments.employee_id
+INNER JOIN enclosures
+ON enclosures.id = assignments.enclosure_id
+WHERE assignments.enclosure_id = 1;
